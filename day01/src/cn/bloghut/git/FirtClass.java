0716b1f5064d1f83f3692dc34187f2e301f1c13c
1765736057 <1765736057@qq.com>
2019-06-01 20:30:39 +0800

    新增了打印

--- day01/src/cn/bloghut/git/FirtClass.java
+++ day01/src/cn/bloghut/git/FirtClass.java
@@ -5,9 +5,11 @@ public class FirtClass {
         System.out.println("hello git   project  ");
         System.out.println("hello git   project  ");
         System.out.println("hello git   project  ");
+        //hello test
     }
     public void test(){
         int i = 0;
         i +=10;
+        System.out.println(i);
     }
 }
